1dda1ff148e90406bb2e2f10a000b7c37c0ec873
Robpol86 <robpol86@gmail.com>
2016-11-30 20:27:17 -0800

    Simplified history. Color ls, no export.
    Appending to eternal history file in PROMPT_COMMAND but using regular
    bash history otherwise. Loads less into memory. Clears up missing
    history mystery.
    
    Enabling ls coloring on OS X and Linux.
    
    Bash completion for loop.
    
    Tidying up.
    
    Not exporting, no need in these bash scripts.

--- bashrc.sh
+++ bashrc.sh
@@ -1,9 +1,17 @@
 # Functions and aliases.
 # https://github.com/Robpol86/dotfiles
 
-alias grep='grep --color=auto' 2>/dev/null
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
 alias lower="tr '[:upper:]' '[:lower:]'"
 alias upper="tr '[:lower:]' '[:upper:]'"
+
+if [[ $OSTYPE == darwin* ]]; then
+    alias ls='ls -G'
+else
+    alias ls='ls --color=auto'
+fi
 
 function _robpol86_run_once {
     git config --global alias.exec '!exec '
@@ -16,7 +24,7 @@ function _robpol86_run_once {
     git config --global user.email robpol86@gmail.com
     git config --global user.name Robpol86
     for path in $(find /usr -type f -name diff-highlight 2>/dev/null)
-    do if [ -f $path ]; then
+    do if [ -f ${path} ]; then
         git config --global pager.diff "perl $path |less"
         git config --global pager.show "perl $path |less"
         git config --global pager.log "perl $path |less"

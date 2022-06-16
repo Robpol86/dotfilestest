04175a2b56837bb21195547bb301e1fe87965fa3
Robpol86 <robpol86@gmail.com>
2022-06-16 11:44:35 -0500

    Fix: OMZ installed by default in codespaces (#2)
    * if else
    
    * fix lint
    
    * -v
    
    * preinstall omz
    
    * park
    
    * wip
    
    * style

--- install.sh
+++ install.sh
@@ -8,7 +8,10 @@ set -o errexit  # Exit script if a command fails.
 set -o nounset  # Treat unset variables as errors and exit immediately.
 set -o pipefail  # Exit script if pipes fail instead of just the last program.
 
+CODESPACES="${CODESPACES:-}"
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ZSH="${ZSH:-"$HOME/.oh-my-zsh"}"
+ZSH_CUSTOM="${ZSH_CUSTOM:-"$ZSH/custom"}"
 
 # Print error to stderr.
 error() {
@@ -39,12 +42,20 @@ symlink() {
 
 # Main function.
 main() {
-    info Installing Oh My Zsh
-    RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    command -v zsh || command "$_"  # Print error if zsh command not found.
+    if [ ! -e "$ZSH" ]; then
+        info Installing Oh My Zsh
+        RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    elif [ -n "$CODESPACES" ]; then
+        info Setting codespace shell to Zsh
+        sudo chsh -s "$(command -v zsh)"
+    else
+        info Setting shell to Zsh
+        chsh -s "$(command -v zsh)"
+    fi
     set -o xtrace  # Print commands before executing them.
 
     info Installing Zsh plugins
-    ZSH_CUSTOM="${ZSH_CUSTOM:-"$HOME/.oh-my-zsh/custom"}"
     git -C "$ZSH_CUSTOM/plugins" clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
     git -C "$ZSH_CUSTOM/plugins" clone --depth=1 https://github.com/so-fancy/diff-so-fancy.git  # Not really a zsh plugin.
     symlink "$HERE/themes/robpol86.zsh-theme" "$ZSH_CUSTOM/themes/robpol86.zsh-theme"

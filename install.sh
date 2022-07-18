ba3e5f4708cbd40d3e6f90d7c0a99f649a31a909
Robpol86 <robpol86@gmail.com>
2022-07-18 11:11:47 -0500

    new approach

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Install dotfiles and changes shell to zsh.
+# Install dotfiles.
 #
-# For use with GitHub Codespaces as well as local development machines.
+# Supported environments:
+#   * GitHub codespaces with zsh
+#   * General purpose machines with zsh (if installed) or bash
 
 set -o errexit  # Exit script if a command fails.
 set -o nounset  # Treat unset variables as errors and exit immediately.
@@ -15,52 +17,83 @@ NAME="$(basename "${BASH_SOURCE[0]}")"
 ZSH="${ZSH:-"$HOME/.oh-my-zsh"}"
 ZSH_CUSTOM="${ZSH_CUSTOM:-"$ZSH/custom"}"
 
-# Main function.
-main() {
-    # Install OMZ.
-    command -v zsh || command "$_"  # Print error if zsh command not found.
-    if [ -n "$CODESPACES" ]; then  # Running from a GitHub codespace. OMZ is pre-installed, just change shell.
-        echo "Changing codespace shell to zsh"
-        sudo chsh -s "$(command -v zsh)" "$USER"
-    elif [ ! -e "$ZSH" ]; then  # OMZ not installed, installing.
-        echo "Installing Oh My Zsh"
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    else  # OMZ already installed.
-        echo "Oh My Zsh is already installed"
+# Configure VS Code defaults if running from GitHub codespace.
+gh_cs_vscode_defaults() {
+    if [ -z "$CODESPACES" ]; then
+        return  # Not running from GitHub codespace.
     fi
-
-    echo "Installing plugins"
-    test -e "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ||
-        git clone --depth=1 "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$_"
-    test -e "$ZSH_CUSTOM/plugins/diff-so-fancy" ||
-        git clone --depth=1 "https://github.com/so-fancy/diff-so-fancy.git" "$_"  # Not really a zsh plugin.
-    echo "Symlinking theme"
-    ln -fsv "$HERE/robpol86.zsh-theme" "$ZSH_CUSTOM/themes/robpol86.zsh-theme"
-
-    echo "Symlinking other files"
-    ln -fsv "$HERE/vimrc" "$HOME/.vimrc"
-    ln -fsv "$HERE/zshrc.sh" "$HOME/.zshrc"
-    ln -fsv "$HERE/zprofile.sh" "$HOME/.zprofile"
-    install -m0700 -d "$HOME/.ssh"
-    ln -fsv "$HERE/ssh_config" "$HOME/.ssh/config"
-
-    echo "Configuring git"
-    zsh -lc "_robpol86_git_config '$ZSH_CUSTOM'"
-}
-
-# Set VS Code defaults if running on a new GitHub Codespaces VM.
-config_vscode() {
     local settings_json="$HOME/.vscode-remote/data/Machine/settings.json"
 
-    echo "Configuring VS Code"
-    jq -rsS ".[0] * .[1]" "$settings_json" "$HERE/settings.json" > "$settings_json.new"
+    echo "Pre-configuring VS Code"
+    jq -rsS ".[0] * .[1]" "$settings_json" "$HERE/settings-gh-cs.json" > "$settings_json.new"
     mv -v "$settings_json.new" "$settings_json"
 
-    echo "Restarting VS Code"
+    # echo "Restarting VS Code"
     # ps ux |grep vscode-remote |awk '{print $2}' |xargs kill  # TODO better
 }
 
-echo "Begin installing dotfiles via $NAME..."
+# Setup shell-agnostic dotfiles.
+dotfiles_common() {
+    echo "Symlinking common dotfiles"
+
+    # SSH
+    if [ -e "$HOME/.ssh/config" ] && [ ! -L "$HOME/.ssh/config" ]; then
+        echo "SSH config exists, aborting" >&2
+        exit 1
+    fi
+    install -m0700 -d "$HOME/.ssh"
+    ln -fsv "$HERE/ssh_config" "$HOME/.ssh/config"
+
+    # VIM
+    ln -fsv "$HERE/vimrc" "$HOME/.vimrc"
+}
+
+# Setup bash dotfiles.
+dotfiles_bash() {
+    :  # TODO
+}
+
+# Setup zsh dotfiles.
+dotfiles_zsh() {
+    :  # TODO cli argument requiring zsh or not?
+}
+
+# Main function.
+main() {
+    echo "Begin installing dotfiles via $NAME..."
+    gh_cs_vscode_defaults
+    dotfiles_common
+    dotfiles_bash
+    dotfiles_zsh
+    echo "Done installing dotfiles via $NAME..."
+
+#    # Install OMZ.
+#    command -v zsh || command "$_"  # Print error if zsh command not found.
+#    if [ -n "$CODESPACES" ]; then  # Running from a GitHub codespace. OMZ is pre-installed, just change shell.
+#        echo "Changing codespace shell to zsh"
+#        sudo chsh -s "$(command -v zsh)" "$USER"
+#    elif [ ! -e "$ZSH" ]; then  # OMZ not installed, installing.
+#        echo "Installing Oh My Zsh"
+#        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#    else  # OMZ already installed.
+#        echo "Oh My Zsh is already installed"
+#    fi
+#
+#    echo "Installing plugins"
+#    test -e "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ||
+#        git clone --depth=1 "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$_"
+#    test -e "$ZSH_CUSTOM/plugins/diff-so-fancy" ||
+#        git clone --depth=1 "https://github.com/so-fancy/diff-so-fancy.git" "$_"  # Not really a zsh plugin.
+#    echo "Symlinking theme"
+#    ln -fsv "$HERE/robpol86.zsh-theme" "$ZSH_CUSTOM/themes/robpol86.zsh-theme"
+#
+#    echo "Symlinking other files"
+#    ln -fsv "$HERE/zshrc.sh" "$HOME/.zshrc"
+#    ln -fsv "$HERE/zprofile.sh" "$HOME/.zprofile"
+#
+#    echo "Configuring git"
+#    zsh -lc "_robpol86_git_config '$ZSH_CUSTOM'"
+}
+
+# Run.
 main
-[ -z "$CODESPACES" ] || config_vscode
-echo "Done installing dotfiles via $NAME..."

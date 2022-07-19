77662d58fe2b6bc735b7e39bcc44dcf8a04f450e
Robpol86 <robpol86@gmail.com>
2022-07-19 11:56:38 -0500

    from github, bash only

--- bash_profile.sh
+++ bash_profile.sh
@@ -3,12 +3,6 @@
 # Environment and startup programs.
 # https://github.com/Robpol86/dotfiles
 
-# If not running interactively, don't do anything.
-case $- in
-    *i*) ;;
-      *) return ;;
-esac
-
 # History.
 HISTFILESIZE=200000
 HISTSIZE=100000
@@ -76,4 +70,34 @@ elif [ $EUID == 0 ]; then  # Linux root.
     PS1='\[\e[1;31m\]\h\[\e[00m\]:\[\e[1;34m\]\W'"$PS1"' \$\[\e[0m\] '
 else  # Linux non-root.
     PS1='\[\e[1;32m\]\u@\h\[\e[00m\]:\[\e[1;34m\]\W'"$PS1"' \$\[\e[0m\] '
+fi
+
+###### GITHUB
+
+# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
+
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
 fi
